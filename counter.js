9234295bbd0bc26a2e1930ba3a576909ac1e203b
initt6 <jckobkz@gmail.com>
2020-12-20 00:53:55 +0100

    [ADD] Added animation of countdown

--- counter.js
+++ counter.js
@@ -1,5 +1,7 @@
 (function() {
     const form = document.forms.date;
+    const clock = document.querySelector('#clock');
+    let oldTime;
     let timer;
     // adds 0 to dates/hours which are < 10
     function transformDate(h) {
@@ -21,9 +23,26 @@
         return today
     }
 
+    // DO SECOND FUNCTION WHICH WILL SUPPORT CLONE AND NORMAL NODE!
+    function animateTransition(e, remove) { 
+        let velocity = parseInt(e.style.top);
+        e.id = `animated-${e.id}`;
+        let animation = setInterval(function() {
+            if (velocity <= -200) {
+                clearInterval(animation);
+                e.remove()
+                return
+            }
+            move();
+        }, 30);
+        function move() {
+            velocity -= 3;
+            e.style.top = `${velocity}px`;
+        }
+    }
+
     //I'm sure that this can be done 10x times better
     function countTime(value) {
-        const clock = document.querySelector('#clock');
         const now = new Date(getNow());
         const date = new Date(value);
         const difference = date-now;
@@ -36,25 +55,75 @@
         if (weeks >= 0 && days >= 0 && minutes >= 0 && seconds >= 0 && hours >= 0) {
             const node = document.querySelector('.time');
             if (!node) {
+
                 for (let entity of time) {
                     let timeElement = document.createElement('div');
-                    timeElement.innerHTML = `${transformDate(entity[1])}`;
                     timeElement.className = 'time';
-                    timeElement.id = entity[0];
+                    timeElement.id = `${entity[0]}-box`;
                     clock.appendChild(timeElement);
-                }
-            } else {
-                for (let entity of time) {
-                let timeBit = document.querySelector(`#${entity[0]}`);
-                    timeBit.innerHTML = `${transformDate(entity[1])}`;
+                    oldTime = new Map();
                 }
             }
+            for (let entity of time){
+                if (oldTime.get(entity[0]) != entity[1]) {
+                    if (document.querySelector(`#${entity[0]}`)){
+                        let previousElement = document.querySelector(`#${entity[0]}`);
+                        if (previousElement.classList.contains('content')) {
+                            console.log(previousElement);
+                            previousElement.className = 'animate';
+                        }
+                    }
+                    let timeContent = document.createElement('div');
+                    let box = document.querySelector(`#${entity[0]}-box`);
+                    timeContent.innerHTML = `${transformDate(entity[1])}`;
+                    timeContent.id = entity[0];
+                    timeContent.style.top = '75px';
+                    timeContent.className = 'content';
+                    box.appendChild(timeContent);
+                }
+            }
+            
+                    /*clock.appendChild(timeElement);
+                    let timeContent = document.createElement('div')
+                    timeContent.innerHTML = `${transformDate(entity[1])}`;
+                    timeContent.className = 'content animate'
+                    timeContent.style.top = '0px';
+                    timeContent.id = entity[0];
+                    timeElement.id = `${entity[0]}-box`;
+                    timeElement.appendChild(timeContent);*/
+                /*for (let entity of time) {
+                    if (oldTime.get(entity[0]) != entity[1]) {
+                        let timeBit = document.querySelector(`#${entity[0]}`);
+                        let nextTimeElement = document.createElement('div');
+                        nextTimeElement.innerHTML = `${transformDate(entity[1])}`;
+                        nextTimeElement.id = entity[0];
+                        nextTimeElement.className = `content`;
+                        nextTimeElement.style.top = '85px';
+                        animateTransition(nextTimeElement, true);
+                        document.querySelector(`#${entity[0]}-box`).append(nextTimeElement);
+                    }
+                }*/
+                oldTime = new Map(time);
+
         }
         if (weeks == 0 && days == 0 && minutes == 0 && seconds == 0 && hours == 0) {
             localStorage.removeItem('date');
             clock.innerHTML = '';
         }
     }
+    window.addEventListener('animationend', () => {
+        if (document.querySelector('.content')) {
+            let animatedElement = document.querySelectorAll('.content');
+            for (el of animatedElement) {
+                el.style.top = '0px';
+            }
+        }
+        if (document.querySelector('.animate')){
+            let secondAnimation = document.querySelector('.animate');
+            secondAnimation.style.top = '-75px';
+            secondAnimation.remove();
+        }
+    });
     form.button.addEventListener('click', () => {
         const date = form.datetime.value;
         if (!localStorage.getItem('date')) {
